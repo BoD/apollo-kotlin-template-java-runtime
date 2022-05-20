22f045e35b9005e51c7fab624cae7832d3ddd152
BoD <BoD@JRAF.org>
2022-05-20 17:15:23 +0200

    Use latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("com.apollographql.apollo3") version "3.2.2"
+    kotlin("jvm") version "1.6.21"
+    id("com.apollographql.apollo3") version "3.3.0"
+    application
 }
 
 group = "org.example"
@@ -8,7 +9,7 @@ version = "1.0.0-SNAPSHOT"
 
 allprojects {
     repositories {
-        mavenLocal()
+//        mavenLocal()
         google()
         mavenCentral()
     }
@@ -16,9 +17,16 @@ allprojects {
 
 
 dependencies {
-    implementation("com.apollographql.apollo3", "apollo-runtime", "3.2.2")
+    implementation("com.apollographql.apollo3", "apollo-runtime", "3.3.0")
 }
 
 apollo {
     packageName.set("com.example")
 }
+
+application {
+    mainClass.set("com.example.MainKt")
+}
+
+// `./gradlew downloadApolloSchema --endpoint='https://apollo-fullstack-tutorial.herokuapp.com/graphql' --schema=`pwd`/src/main/graphql/schema.graphqls` to download the schema
+// `./gradlew run` to run the app
